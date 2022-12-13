a0b6310d816b9da9e725c58c3fbc28c24785ba1f
achyut kadam <achyut.k@acldigital.com>
2022-12-13 13:34:59 +0530

    Address URLs files

--- README.md
+++ README.md
@@ -18,16 +18,16 @@ python3 manage.py startapp account_type
 python3 manage.py startapp user_information
 python3 manage.py startapp particular_detail
 
-touch card/forms.py
-touch user/forms.py
-touch salary/forms.py
-touch manager/forms.py
-touch address/forms.py
-touch nominee/forms.py
-touch customer/forms.py
-touch employee/forms.py
-touch particular/forms.py
-touch department/forms.py
-touch account_type/forms.py
-touch user_information/forms.py
-touch particular_detail/forms.py
+touch card/forms.py card/urls.py
+touch user/forms.py user/urls.py
+touch salary/forms.py salary/urls.py
+touch manager/forms.py manager/urls.py
+touch address/forms.py address/urls.py
+touch nominee/forms.py nominee/urls.py
+touch customer/forms.py customer/urls.py
+touch employee/forms.py employee/urls.py
+touch particular/forms.py particular/urls.py
+touch department/forms.py department/urls.py
+touch account_type/forms.py account_type/urls.py
+touch user_information/forms.py user_information/urls.py
+touch particular_detail/forms.py particular_detail/urls.py
--- address/views.py
+++ address/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from address.forms import AddressForm
+from address.models import Address
 # Create your views here.
+def emp(request):
+  if request.method == "POST":
+    form = AddressForm(request.POST)
+    if form.is_valid():
+      try:
+          form.save()
+        return redirect('/show')
+      except:
+        pass
+  else:
+    form = AddressForm()
+  return render(request,'index.html',{'form':form})
+def show(request):
+  addresses = Address.objects.all()
+  return render(request,"show.html",{'addresses':addresses})
+def edit(request, id):
+  address = Address.objects.get(id=id)
+  return render(request,'edit.html', {'address':address})
+def update(request, id):
+  address = Address.objects.get(id=id)
+  form = AddressForm(request.POST, instance = address)
+  if form.is_valid():
+    form.save()
+    return redirect("/show")
+  return render(request, 'edit.html', {'address': address})
+def destroy(request, id):
+  address = Address.objects.get(id=id)
+  address.delete()
+  return redirect("/show")

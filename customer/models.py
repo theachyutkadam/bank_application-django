539f4b39683cd4508fd140aa4aee4157494fd0f7
achyut kadam <achyut.k@acldigital.com>
2022-12-20 11:44:31 +0530

    Create model of customer

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from account_type.models import AccountType
 
-# Create your models here.
+
+class Customer(models.Model):
+    account_type = models.ForeignKey(AccountType, on_delete=models.CASCADE)
+    account_number = models.CharField(max_length=15)
+    amount_limit = models.FloatField()
+    current_balance = models.FloatField()
+
+    class Meta:
+        db_table = "customer"
--- customer/views.py
+++ customer/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from customer.forms import CustomerForm
+from customer.models import Customer
 # Create your views here.
+
+
+def index(request):
+    customers = Customer.objects.all()
+    return render(request, "customer/index.html", {'customers': customers})
+
+
+def create(request):
+    if request.method == "POST":
+        form = CustomerForm(request.POST)
+        if form.is_valid():
+            form.save()
+            department = Department.objects.last()
+            return render(request, 'customer/show.html', {'customer': department})
+    else:
+        form = CustomerForm()
+    return render(request, 'customer/new.html')
+
+
+def show(request, id):
+    customer = Customer.objects.get(id=id)
+    return render(request, "customer/show.html", {'customer': customer})
+
+
+def new(request):
+    return render(request, "customer/new.html")
+
+
+def edit(request, id):
+    customer = Customer.objects.get(id=id)
+    return render(request, 'customer/edit.html', {'customer': customer})
+
+
+def update(request, id):
+    customer = Customer.objects.get(id=id)
+    customer.name = request.POST['name']
+    customer.loan_intrest_rate = request.POST['loan_intrest_rate']
+    customer.saving_intrest_rate = request.POST['saving_intrest_rate']
+    customer.save()
+    return render(request, "customer/show.html", {'customer': customer})
+
+
+def destroy(request, id):
+    customer = Customer.objects.get(id=id)
+    customer.delete()
+    return redirect("/customer")
